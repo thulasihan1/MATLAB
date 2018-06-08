7dea6d291a72e8451100922023a92e21fa03dded
Thulasihan Sivarajah <39424972+thulasihan1@users.noreply.github.com>
2018-06-08 15:47:58 -0400

    Add files via upload

---
+++ matlabassignment1.m
@@ -0,0 +1,7 @@
+A=[1 2 5; 3 3 8; 2 4 9]   %Matrix A
+B= [ 2 3 4; 2 6 7; 1 4 8] %Matrix B
+C= A+B                    %Addition of matrix A and matrix B
+D= A*B                    %Multiplication of matrix A and matrix B
+E= A'*A                   %Multiplication of transpose A and A by itself
+det(A)                    % Determinant of A
+inv(A)                    %Determinant of B
